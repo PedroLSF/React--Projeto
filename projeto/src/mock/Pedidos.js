f2f4328cd1e7a6ba2600818b112021f81acbd7c4
pedrolucas0 <siqueirafernandes02@gmail.com>
2023-04-08 20:55:22 -0300

    Mock

---
+++ projeto/src/mock/Pedidos.js
@@ -0,0 +1,34 @@
+const pedidosMock = [
+
+    {
+        _id: "1",
+        nome: "Pepperoni",
+        descricao: "Pepperoni Pizza",
+        preco: 29,
+        imagem: "https://e7.pngegg.com/pngimages/935/770/png-clipart-pizza-pizza.png"
+    },
+    {
+        _id: "2",
+        nome: "Margueritte",
+        descricao: "Margueritte Pizza",
+        preco: 29,
+        imagem: "https://img2.gratispng.com/20180608/ubk/kisspng-california-style-pizza-sicilian-pizza-pizza-marghe-pizza-mar-del-5b1ad887c64621.1746068715284860238121.jpg"
+    },
+    {
+        _id: "3",
+        nome: "Coke",
+        descricao: "Soda",
+        preco: 9,
+        imagem: "https://www.imagensempng.com.br/wp-content/uploads/2021/03/Coca-Cola-Copo-Png-1024x1024.png"
+    },
+    {
+        _id: "4",
+        nome: "Sprite",
+        descricao: "Soda",
+        preco: 9,
+        imagem: "https://img2.gratispng.com/20180201/uoq/kisspng-soft-drink-sprite-coca-cola-fanta-pepsi-sprite-can-5a73b18043a639.7902954015175315202771.jpg"
+    }
+
+]
+
+export default pedidosMock;
